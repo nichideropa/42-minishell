2e1a726b6773c6956fd9d9b9d4cec86eeb93df04
root <root@DESKTOP-V5M1H7P>
2025-05-09 19:59:44 +0200

    fix compilation error

--- improved_version/inc/minishell.h
+++ improved_version/inc/minishell.h
@@ -64,7 +64,7 @@ typedef struct s_quotes
 	int	d;
 }	t_quotes;
 
-t_minishell	g_msh;
+extern t_minishell	g_msh;
 
 /* ************************************************************************** */
 /* FUNCTION PROTOTYPES														  */
